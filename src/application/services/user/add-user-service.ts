d3f971f81ab356e50e51e80b5ba7920663286bdb
LucasGSilva2102 <lucasgsilva2102@gmail.com>
2021-12-27 13:38:57 -0300

    feat: create add user service

---
+++ src/application/services/user/add-user-service.ts
@@ -0,0 +1,16 @@
+import { User } from '../../../domain/models/user';
+import { CreateUserDTO } from '../../../domain/dto/user';
+import { AddUserUseCases } from '../../../domain/usecases/user';
+import { AddUserRepository } from '../../repositories/user';
+
+export class AddUserService implements AddUserUseCases {
+  private addUserRepository: AddUserRepository;
+
+  constructor(addUserRepository: AddUserRepository) {
+    this.addUserRepository = addUserRepository;
+  }
+
+  async add(userData: CreateUserDTO): Promise<User> {
+    return this.addUserRepository.add(userData);
+  }
+}
