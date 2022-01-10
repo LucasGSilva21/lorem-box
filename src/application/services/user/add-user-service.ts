714c62d94f8c07e5794fd4ea144d1c1b96fdff91
LucasGSilva2102 <lucasgsilva2102@gmail.com>
2022-01-10 16:57:37 -0300

    feat: ensure add a new user with encrypted password

--- src/application/services/user/add-user-service.ts
+++ src/application/services/user/add-user-service.ts
@@ -2,15 +2,24 @@ import { User } from '../../../domain/models/user';
 import { CreateUserDTO } from '../../../domain/dto/user';
 import { AddUserUseCases } from '../../../domain/usecases/user';
 import { AddUserRepository } from '../../interfaces/repositories/user';
+import { Hasher } from '../../interfaces/criptography';
 
 export class AddUserService implements AddUserUseCases {
-  private addUserRepository: AddUserRepository;
+  private readonly addUserRepository: AddUserRepository;
 
-  constructor(addUserRepository: AddUserRepository) {
+  private readonly hasher: Hasher;
+
+  constructor(addUserRepository: AddUserRepository, hasher: Hasher) {
     this.addUserRepository = addUserRepository;
+    this.hasher = hasher;
   }
 
   async add(userData: CreateUserDTO): Promise<User> {
-    return this.addUserRepository.add(userData);
+    const hashedPassword = await this.hasher.hash(userData.password);
+
+    return this.addUserRepository.add({
+      ...userData,
+      password: hashedPassword,
+    });
   }
 }
